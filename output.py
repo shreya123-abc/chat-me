b6ccdbfc5b48813f508974c8fdf8cc7f674ae39d
shreya123-abc <114906079+shreya123-abc@users.noreply.github.com>
2023-03-20 12:36:40 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,50 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: C:/Users/HP/chat-me.py =======================
+You: hi
+Bot: Hello!
+You: how are you doing
+Bot: I'm doing fine.
+You: i love to code here
+Bot: Thank you!
+You: when are you available
+Bot: We are available 24 hrs for your help.How are you feeling?
+ 1.Hungry
+ 2.Wanna Study
+ 3.Wanna Listen music
+ 4.Entertainment
+You: 1
+Bot: You may Order your favourites from Swiggy or Zomato.
+Swiggy:https://www.swiggy.com/
+Zomato:https://www.zomato.com/
+You: 2
+Bot: You may use geeksforgeeks or javatpoint to learn new.
+geeksforgeeks: https://www.geeksforgeeks.org/
+javatpoint:https://www.javatpoint.com/
+You: 3
+Bot: You may Listen your favourites from ganna or spotify.
+ganna: https://gaana.com/
+ spotify: https://open.spotify.com/
+You: 4
+Bot: (a) News
+(b)Movies
+(c)Sports
+You: a
+Bot: You may contact to different news channels like ndtv or economic times.
+ NDTV : https://www.ndtv.com/
+ THE ECONOMIC TIMES : https://economictimes.indiatimes.com/
+You: b
+Bot: You may connect with Netflix Amazon prime.
+Netflix : https://www.netflix.com/in/
+ Amazon Prime : https://www.primevideo.com/
+You: c
+Bot: You may use sports18 or sky sports.
+Sports18 : https://www.sports18.com/
+Sky Sports : https://www.skysports.com/
+You: do you wanna eat something?
+Bot: I don't like eating anything because I'm a bot obviously!
+You: thank you
+Bot: You're welcome!
+You: okk bye
+Bot: See you!
